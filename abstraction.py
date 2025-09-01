a1073fb2ee58ce78231f90043884db38531cb062
Avnish Singh <avnishsingh150606@gmail.com>
2025-09-01 18:41:25 +0530

    Add files via upload

---
+++ abstraction.py
@@ -0,0 +1,12 @@
+from abc import ABC, abstractmethod 
+class Vehicle(ABC):
+    @abstractmethod
+    def start_engine(self):
+        pass
+
+class Car(Vehicle):
+    def start_engine(self):
+        print("Car engine started")
+
+obj = Car()
+obj.start_engine() # output: Car engine started
