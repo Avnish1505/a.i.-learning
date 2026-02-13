6dadd2962e7a412446061b5e8f49941ee822b677
Avnish Singh <avnishsingh150606@gmail.com>
2026-02-13 12:34:56 +0530

    Uncomment and fix syntax errors in tech2.py
    Fix syntax errors and uncomment code for various checks including number, age, and temperature evaluations.

--- tech2.py
+++ tech2.py
@@ -1,65 +1,65 @@
-# num = int(input("Enter a number "))
-# if num == 0:
-#     print("zero")
-# elif num > 0
-#     print("positive")
-#     if num % 2 == 0:
-#         print("even")
-#     else:
-#         print("odd")
-# person_age = int(input("Enter you age "))
-# if person_age >= 18:
-#    print("You are the eligible to vote")
-# else:
-#     print("You are not eligible to vote")
-# num1 = int(input("Enter a number: "))
-# num2 = int(input("Enter another number:"))
-# if num1 > 0:
-#   print("valid")
-# if num1 > num2:
-#      print("num1 is greater than num2")
-# elif num1 == num2:
-#     print("both are equal")
-# student_marks = int(input("Enter your marks:"))
-# if student_marks >= 90:
-#     print("A grade")
-# elif student_marks >= 80:
-#     print("B grade")
-# year = int(input("Enter a year:"))
-# if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
-#     print("leap year")
-# num = int(input("Enter a number: "))
-# if num < 10:
-#    print("number is single digit")
-# elif num < 100:
-#    print("number is double digit")
-# elif num < 1000:
-#    print("number is three digit")
-# else:
-#    print("number is four digit or more")
-# day_number = int(input("Enter day number(1-7): "))
-# if day_number == 1:
-#     print("Monday")
-# elif day_number == 2:
-#     print("Tuesday")
-# elif day_number == 3:
-#     print("Wednesday")
-# elif day_number == 4:
-#     print("Thursday")
-# elif day_number == 5:
-#     print("Friday")
-# elif day_number == 6:
-#     print("Saturday")
-# elif day_number == 7:
-#     print("Sunday")
-# else:
-#     print("Invalid day number")
-# temperature = int(input("Enter temperature in celsius:"))
-# if temperature > 0:
-#     print("freezing point")
-# elif temperature == 0-20:
-#     print("cold")
-# elif temperature == 21-35:
-#     print("warm")
-# elif temperature > 35:
+num = int(input("Enter a number "))
+if num == 0:
+    print("zero")
+elif num > 0
+    print("positive")
+    if num % 2 == 0:
+        print("even")
+    else:
+        print("odd")
+person_age = int(input("Enter you age "))
+if person_age >= 18:
+   print("You are the eligible to vote")
+else:
+    print("You are not eligible to vote")
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number:"))
+if num1 > 0:
+  print("valid")
+if num1 > num2:
+     print("num1 is greater than num2")
+elif num1 == num2:
+    print("both are equal")
+student_marks = int(input("Enter your marks:"))
+if student_marks >= 90:
+    print("A grade")
+elif student_marks >= 80:
+    print("B grade")
+year = int(input("Enter a year:"))
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print("leap year")
+num = int(input("Enter a number: "))
+if num < 10:
+   print("number is single digit")
+elif num < 100:
+   print("number is double digit")
+elif num < 1000:
+   print("number is three digit")
+else:
+   print("number is four digit or more")
+day_number = int(input("Enter day number(1-7): "))
+if day_number == 1:
+    print("Monday")
+elif day_number == 2:
+    print("Tuesday")
+elif day_number == 3:
+    print("Wednesday")
+elif day_number == 4:
+    print("Thursday")
+elif day_number == 5:
+    print("Friday")
+elif day_number == 6:
+    print("Saturday")
+elif day_number == 7:
+    print("Sunday")
+else:
+    print("Invalid day number")
+temperature = int(input("Enter temperature in celsius:"))
+if temperature > 0:
+    print("freezing point")
+elif temperature == 0-20:
+    print("cold")
+elif temperature == 21-35:
+    print("warm")
+elif temperature > 35:
 #     print("hot")
