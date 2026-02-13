8da99e4ccee285e8224db5c2f3773abcdd25350b
Avnish Singh <avnishsingh150606@gmail.com>
2026-02-13 12:33:59 +0530

    Uncommented class definitions and method calls

--- encapsulation.py
+++ encapsulation.py
@@ -1,55 +1,55 @@
-# class BankAccount:
-#     def __init__(self, owner, balance):
-#         self.owner = owner
-#         self.__balance = balance   
+class BankAccount:
+    def __init__(self, owner, balance):
+        self.owner = owner
+        self.__balance = balance   
 
-#     def deposit(self, amount):     
-#         self.__balance += amount
+    def deposit(self, amount):     
+        self.__balance += amount
 
-#     def get_balance(self):         
-#         return self.__balance      
+    def get_balance(self):         
+        return self.__balance      
 
-# acc = BankAccount("Alice", 10000)
-# acc.deposit(500)
-# print(acc.get_balance())
+acc = BankAccount("Alice", 10000)
+acc.deposit(500)
+print(acc.get_balance())
 
-# class Student:
-#     def __init__(self, marks , name): # private variable 
-#         self.__marks = marks
-#         self.__name = name
+class Student:
+    def __init__(self, marks , name): # private variable 
+        self.__marks = marks
+        self.__name = name
 
-#     def get_marks(self):
-#         return self.__marks
+    def get_marks(self):
+        return self.__marks
     
-# result = Student(85, "Avnish")
-# print(result.get_marks())
+result = Student(85, "Avnish")
+print(result.get_marks())
 
-# class Employee:
-#     def __init__(self , name , salary): #private variable
-#         self.name = name 
-#         self.salary = salary 
+class Employee:
+    def __init__(self , name , salary): #private variable
+        self.name = name 
+        self.salary = salary 
 
-#     def increase_salary(self):
-#         self.salary = self.salary + (self.salary * 0.10)
-#         return self.salary 
+    def increase_salary(self):
+        self.salary = self.salary + (self.salary * 0.10)
+        return self.salary 
 
-# em1 = Employee("Avnish", 50000)
-# print("salary before increase:", em1.salary)
-# print("salary after increase:", em1.increase_salary())
+em1 = Employee("Avnish", 50000)
+print("salary before increase:", em1.salary)
+print("salary after increase:", em1.increase_salary())
 
-# class Mobile:
-#     def __init__(self, brand, price):
-#         self.brand = brand
-#         self.price = price
+class Mobile:
+    def __init__(self, brand, price):
+        self.brand = brand
+        self.price = price
 
-#     def price_change(self, new_price):
-#         self.price = new_price
+    def price_change(self, new_price):
+        self.price = new_price
 
-# mobile = Mobile("Apple", 100000)
-# print("Brand:", mobile.brand)
-# print("price:", mobile.price)
-# mobile.price_change(90000)
-# print("price after change:", mobile.price)
+mobile = Mobile("Apple", 100000)
+print("Brand:", mobile.brand)
+print("price:", mobile.price)
+mobile.price_change(90000)
+print("price after change:", mobile.price)
 
 
 
